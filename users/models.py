e15697a09732af9520e3fc8570c130a2d48fa03d
joelbenson20 <joelbenson20@gmail.com>
2026-03-28 20:49:58 -0500

    cleaned up database

--- users/models.py
+++ users/models.py
@@ -8,6 +8,10 @@ class User(AbstractUser):
     def get_absolute_url(self):
         return reverse('user', kwargs={'username': self.username})
     
+    @property
+    def voted_webpage_ids(self):
+        return set(self.webpage_votes.values_list('webpage_id', flat=True))
+    
 class Comment(models.Model):
     user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
     webpage = models.ForeignKey('webpages.Webpage', related_name='webpage_comments', on_delete=models.CASCADE)
@@ -22,9 +26,6 @@ class Comment(models.Model):
 
 class Vote(models.Model):
 
-    value = models.IntegerField(
-        choices=[(1, 'Upvote'), (-1, 'Downvote')]
-    )
     datetime = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -33,10 +34,10 @@ class Vote(models.Model):
 class WebpageVote(Vote):
 
     user = models.ForeignKey(User, related_name='webpage_votes', on_delete=models.CASCADE)
-    webpage = models.ForeignKey(Webpage, related_name='webpage_votes', on_delete=models.CASCADE)
+    webpage = models.ForeignKey(Webpage, related_name='votes', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user} Webpage: {self.webpage}, Vote: {self.value}"
+        return f"{self.user} : {self.webpage}"
     
     class Meta:
         unique_together = ('user', 'webpage')
@@ -44,10 +45,10 @@ class WebpageVote(Vote):
 class CommentVote(Vote):
     
     user = models.ForeignKey(User, related_name='comment_votes', on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, related_name='comment_votes', on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, related_name='votes', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user} Comment: {self.comment}, Vote: {self.value}"
+        return f"{self.user} : {self.comment}"
     
     class Meta:
         unique_together = ('user', 'comment')
