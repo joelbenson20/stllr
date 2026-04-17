5ab09e85a467d1424a565e1c3f24900b09b19c99
joelbenson20 <joelbenson20@gmail.com>
2026-04-17 13:54:07 -0500

    Pages admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Page, PageVote
 
-admin.site.register(Page)
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['canonical', 'title', 'type',]
+    list_filter = ['type', 'tags', 'site_name']
+    search_fields = ['canonical', 'title', 'site_name']
+    ordering = ['canonical']
+    show_facets = admin.ShowFacets.ALWAYS
+
 admin.site.register(PageVote)
