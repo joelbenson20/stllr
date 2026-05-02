cdd73d5e68b2488d0a0b616a6480f2de19f3bb29
Joel Benson <joelbenson20@gmail.com>
2026-05-01 23:11:43 -0500

    allauth templates, email confirmation

--- stllr/stllr/urls.py
+++ stllr/stllr/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls')),
     path('', include('pages.urls', namespace='pages')),
     path('comments/', include('comments.urls', namespace='comments')),
