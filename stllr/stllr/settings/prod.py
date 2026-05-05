c1097e13cd6c29f6037b7b8820a02eebd348f29f
Joel Benson <joelbenson20@gmail.com>
2026-05-05 15:53:32 -0500

    Resend Email API

--- stllr/stllr/settings/prod.py
+++ stllr/stllr/settings/prod.py
@@ -13,12 +13,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/prod'
 
 ADMINS = [
-    ('Stllr Admin', config('EMAIL_HOST_USER'))
+    ('Stllr Admin', config('ADMIN_EMAIL'))
 ]
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = 'anymail.backends.resend.EmailBackend'
+ANYMAIL = {'RESEND_API_KEY': config('RESEND_API_KEY')}
+DEFAULT_FROM_EMAIL = 'noreply@stllr.io'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('ADMIN_EMAIL')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+# EMAIL_TIMEOUT = 10
