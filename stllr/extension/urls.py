75aff87c511e9c43dc6e16f3a6f05a8b54b6e6b5
Joel Benson <joelbenson20@gmail.com>
2026-05-13 03:17:18 -0500

    tabulated extension view

--- stllr/extension/urls.py
+++ stllr/extension/urls.py
@@ -5,7 +5,4 @@ app_name = 'extension'
 
 urlpatterns = [
     path('', views.extension, name='extension'),
-    path('forum/', views.forum, name='forum'),
-    path('room/', views.room, name='room'),
-    path('similar/', views.similar, name='similar'),
 ]
--- stllr/extension/views.py
+++ stllr/extension/views.py
@@ -1,25 +1,20 @@
 from django.http import JsonResponse
 from django.template.loader import render_to_string
-from django.middleware.csrf import get_token
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from pages.models import Page, Domain
 import json
 from pages.utils import get_canonical, get_meta, get_domain_name, verify_security, InsecureURLError
-from django.db.models import Count
 
 
 @csrf_exempt
 @login_required
 @require_POST
 def extension(request):
-    
-    posted_data = json.loads(request.body).get('pageData')
-    url = posted_data.get('url')
-    canonical = get_canonical(url)
-    domain_name = get_domain_name(url)
 
+    data = json.loads(request.body).get('pageData')
+    canonical = get_canonical(data.get('url'))
     try:
         page = Page.objects.get(canonical=canonical)
         if not page.is_active:
@@ -30,17 +25,17 @@ def extension(request):
                 }
             )
     except Page.DoesNotExist:
-        scraped_data = get_meta(url, posted_data.get('head'))
-        title = posted_data.get('title') or scraped_data['title']
+        scraped_data = get_meta(data.get('url'), data.get('head'))
+        title = data.get('title') or scraped_data['title']
         type = scraped_data['type']
         description = scraped_data['description']
-        inner_text = posted_data.get('innerText')
+        inner_text = data.get('innerText')
         image_url = scraped_data['image_url']
-        fav_icon_url = posted_data.get('favIconUrl') or scraped_data['fav_icon_url']
+        fav_icon_url = data.get('favIconUrl') or scraped_data['fav_icon_url']
         site_name = scraped_data.get('site_name')
 
         try:
-            verify_security(url) 
+            verify_security(data.get('url')) 
             verify_security(image_url)
             verify_security(fav_icon_url)
         except InsecureURLError as e:
@@ -51,7 +46,7 @@ def extension(request):
                 }
             )
 
-
+        domain_name = get_domain_name(data.get('url'))
         domain, _ = Domain.objects.get_or_create(name=domain_name)
         if site_name and not domain.site_name:
             domain.site_name = site_name
@@ -70,132 +65,22 @@ def extension(request):
             inner_text=inner_text or scraped_data['inner_text'] or '',
         )
 
-    page_tags_ids = page.tags.values_list('id', flat=True)
-    similar_pages = Page.objects.filter(
-        tags__in=page_tags_ids
-    ).exclude(id=page.id)
-    similar_pages = similar_pages.annotate(
-        same_tags=Count('tags')
-    ).order_by('-same_tags')[:3]
+    tab = request.GET.get('tab')
     
     context = {
         'page': page,
-        'similar_pages': similar_pages,
-        'section': 'forum'
+        'tab': tab
     }
-    
+
+    html = None
+    if (tab == 'forum'):
+        html = render_to_string('extension/forum.html', context=context, request=request)
+    elif (tab == 'room'):
+        html = render_to_string('extension/room.html', context=context, request=request)
+    elif (tab == 'similar'):
+        html = render_to_string('extension/similar.html', context=context, request=request)
+
     return JsonResponse({
         'status': '200',
-        'html': render_to_string('extension/forum.html', context=context, request=request)
+        'html': html
     })
-
-@csrf_exempt
-@require_POST
-def forum(request):
-
-    posted_data = json.loads(request.body).get('pageData')
-    url = posted_data.get('url')
-    canonical = get_canonical(url)
-
-    try:
-        page = Page.objects.get(canonical=canonical)
-        if not page.is_active:
-            return JsonResponse(
-                {
-                    'status': '403',
-                    'html': render_to_string('extension/errors/inactive.html')
-                }
-            )
-    except Page.DoesNotExist:
-        return JsonResponse(
-            {
-                'status': '404',
-                'html': render_to_string('extension/errors/undiscovered.html')
-            }
-        )
-    
-    context = {
-        'page': page,
-        'section': 'forum'
-    }
-
-    return JsonResponse(
-        {
-            'status': '200',
-            'html': render_to_string('extension/forum.html', context=context, request=request)
-        }
-    )
-
-@csrf_exempt
-@require_POST
-def room(request):
-
-    posted_data = json.loads(request.body).get('pageData')
-    url = posted_data.get('url')
-    canonical = get_canonical(url)
-
-    try:
-        page = Page.objects.get(canonical=canonical)
-        if not page.is_active:
-            return JsonResponse(
-                {
-                    'status': '403',
-                    'html': render_to_string('extension/errors/inactive.html')
-                }
-            )
-    except Page.DoesNotExist:
-        return JsonResponse(
-            {
-                'status': '404',
-                'html': render_to_string('extension/errors/undiscovered.html')
-            }
-        )
-    
-    context = {
-        'page': page,
-        'section': 'room'
-    }
-
-    return JsonResponse(
-        {
-            'status': '200',
-            'html': render_to_string('extension/room.html', context=context, request=request)
-        }
-    )
-
-@csrf_exempt
-@require_POST
-def similar(request):
-
-    posted_data = json.loads(request.body).get('pageData')
-    url = posted_data.get('url')
-    canonical = get_canonical(url)
-
-    try:
-        page = Page.objects.get(canonical=canonical)
-        if not page.is_active:
-            return JsonResponse(
-                {
-                    'status': '403',
-                    'html': render_to_string('extension/errors/inactive.html')
-                }
-            )
-    except Page.DoesNotExist:
-        return JsonResponse(
-            {
-                'status': '404',
-                'html': render_to_string('extension/errors/undiscovered.html')
-            }
-        )
-    
-    context = {
-        'page': page,
-        'section': 'similar'
-    }
-
-    return JsonResponse(
-        {
-            'status': '200',
-            'html': render_to_string('extension/similar.html', context=context, request=request)
-        }
-    )
