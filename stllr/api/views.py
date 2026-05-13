501825be2071ede0d59f01b47139c8030c4cd8bc
Joel Benson <joelbenson20@gmail.com>
2026-05-13 15:28:28 -0500

    cleaned up comment tree

--- stllr/api/views.py
+++ stllr/api/views.py
@@ -47,7 +47,7 @@ def create_comment(request):
         response = {
             'status': '201',
             'commentId': new_comment.id,
-            'comment': render_to_string('comments/comment.html', {'comment': new_comment}, request=request)
+            'comment': render_to_string('comment/tree.html', {'comments': [new_comment]}, request=request)
         }
         return (JsonResponse(response))
     return JsonResponse({'status': '400'})
