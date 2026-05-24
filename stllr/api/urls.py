1059f7b728969de100d9d074fc905a1b7f7fb0f9
Joel Benson <joelbenson20@gmail.com>
2026-05-24 00:25:28 -0500

    remove post button, url, view

--- stllr/api/urls.py
+++ stllr/api/urls.py
@@ -6,6 +6,7 @@ app_name = 'api'
 urlpatterns = [
     path('markdownify/', views.markdownify, name='markdownify'),
     path('create/post/', views.create_post, name='create_post'),
+    path('remove/post/', views.remove_post, name='remove_post'),
     path('star/page/', views.star_page, name='star_page'),
     path('star/post/', views.star_post, name='star_post'),
     path('count/room/', views.get_room_count, name='get_room_count'),
